b328bc11bd1a4b094feedbd4e3ec04b50aed9bb9
kouchao <admin@jskou.com>
2021-11-25 23:10:44 +0800

    refactor: 基础参数

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,15 @@ const axios = require('axios')
 
 // 发送请求
 const request = async (url, params) => {
-  const res = await axios(url, { params })
+  const res = await axios(url, {
+    params: {
+      product: 'EFund',
+      deviceid: '874C427C-7C24-4980-A835-66FD40B67605',
+      plat: 'Iphone',
+      version: '6.4.9',
+      ...params,
+    },
+  })
   return res.data
 }
 
