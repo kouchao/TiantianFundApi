4541e3b0651c27a4f69a13edfab3895e4e87cdd0
kouchao <admin@jskou.com>
2021-11-25 23:10:03 +0800

    refactor: 修改基金公司列表接口

--- src/module/fundCompanyBaseList.js
+++ src/module/fundCompanyBaseList.js
@@ -1,9 +1,9 @@
-const { jsonp } = require('../utils/index.js')
+const { request } = require('../utils/index.js')
 
 /**
  * 获取基金公司列表（所有）
  */
 module.exports = async () => {
-  const url = 'https://m.1234567.com.cn/data/FundCompanyBaseList.js'
-  return jsonp(url, 'FundCompanyBaseList')
+  const url = 'https://fundmobapi.eastmoney.com/FundMApi/FundCompanyBaseList.ashx'
+  return request(url)
 }
