6f4a6a628c67dd0a870127b639926502820c70ea
寇超 <kouchao@xindong.com>
2022-05-28 01:16:07 +0800

    test: 搜索接口用例

---
+++ test/search.spec.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+const axios = require('axios')
+const host = 'http://localhost:3001'
+
+describe('搜索接口', function () {
+  it('获取基金详情', async function () {
+    const res = await axios(`${host}/fundSearch`, {
+      params: {
+        key: '新能源',
+        m: 1
+      }
+    })
+    assert.ok(res.data.Datas.length);
+  });
+
+  it('获取基金列表（按字母）', async function () {
+    const res = await axios(`${host}/fundNetList`)
+    assert.ok(res.data.Datas.length);
+  });
+
+  it('获取基金列表（按类型）', async function () {
+    const res = await axios(`${host}/fundMNNetNewList`)
+    assert.ok(res.data.Datas.length);
+  });
+
+  it('获取基金列表(所有-简单)', async function () {
+    const res = await axios(`${host}/fundSuggestList`)
+    assert.ok(res.data.Datas.length);
+  });
+
+  it('获取基金公司列表（所有）', async function () {
+    const res = await axios(`${host}/fundCompanyBaseList`)
+    assert.ok(res.data.Datas.length);
+  });
+
+  it('获取基金公司列表（所有）', async function () {
+    const res = await axios(`${host}/fundCompanyBaseList`)
+    assert.ok(res.data.Datas.length);
+  });
+
+});
