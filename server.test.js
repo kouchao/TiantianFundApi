c49d589dcb5040af9032993b2572845dd526b3c7
寇超 <kouchao@xindong.com>
2022-05-27 20:03:04 +0800

    fix: 测试报告失败

--- server.test.js
+++ server.test.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const path = require('path')
-const serverLoad = require('./index')
+const startServe = require('./index')
 let server
 before(async () => {
-  server = await serverLoad
+  server = await startServe()
 })
 
 after((done) => {
