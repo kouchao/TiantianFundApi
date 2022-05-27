ad55ed897ed38e61151024c1bea5b18bdaa04c18
寇超 <kouchao@xindong.com>
2022-05-28 01:27:34 +0800

    test: 大数据接口用例

---
+++ test/data.spec.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const axios = require('axios')
+const host = 'http://localhost:3001'
+
+describe('大数据接口', function () {
+  it('获取大数据榜单', async function () {
+    const res = await axios(`${host}/bigDataList`)
+    assert.ok(res.data.datas.length);
+  });
+
+  it('获取大数据榜单数据详情', async function () {
+    const res = await axios(`${host}/bigDataDetail`, {
+      params: {
+        cltype: '001'
+      }
+    })
+    assert.equal(res.data.datas.ClType, '001');
+  });
+
+});
