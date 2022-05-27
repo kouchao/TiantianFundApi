1b2c010f3ff78a239dc4a7500e6433766fc1d183
寇超 <kouchao@xindong.com>
2022-05-27 20:17:35 +0800

    refactor: 测试服务启动

---
+++ test/fixtures.js
@@ -0,0 +1,16 @@
+const {startServe} = require("../src/app");
+let server
+
+async function mochaGlobalSetup() {
+  server = await startServe();
+};
+
+async function mochaGlobalTeardown() {
+  await server.close();
+};
+
+module.exports = {
+  mochaGlobalSetup,
+  mochaGlobalTeardown
+}
+
